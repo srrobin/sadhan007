5c6db1036edd6d1536be79c9157a6d4f098ecb73
srrobin <srrobin07@gmail.com>
2023-01-06 18:34:11 +0600

    update

--- src/components/Possession.jsx
+++ src/components/Possession.jsx
@@ -4,12 +4,12 @@ import { content } from "../Content";
 const Possession = () => {
   const { Contact } = content;
   return (
-    <section className="w-full h-screen relative" id="contact">
-      <img src={Bgcontact} alt="" className="w-full h-full object-cover" />
+    <section className="h-[235px] md:w-full md:h-screen relative" id="contact">
+      <img src={Bgcontact} alt="" className="w-full h-full object-contain md:object-cover" />
 
       <div className="absolute top-0 left-0 w-full h-full bg-blue-900/80"></div>
-      <div className="absolute text-white p-4 top-0 w-full h-full flex flex-col justify-center text-center">
-        <h3 className="capitalize text-white pb-5  text-7xl">
+      <div className="absolute text-white p-4 top-0 w-full h-full flex flex-col justify-center text-right md:text-center">
+        <h3 className="capitalize text-white pt-36 md:pl-[600px]  md:pt-96 pb-5 text-xl align-bottom md:text-7xl md:top-[700px]">
           Keep Calm & carry On.
         </h3>
         {/* <p className="text-white text-lg">
